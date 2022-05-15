22108d71496e5196809c22111d16973e3dae386c
Sharique <syedshariq05@gmail.com>
2022-05-15 19:16:26 +0500

    store added

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,29 @@
 import Head from 'next/head'
+
+import { wrapper, store } from "../store/store";
+import { Provider } from "react-redux";
+
 import 'bootstrap/dist/css/bootstrap.css'
-import Layout from '../Components/Layout';
 import("bootstrap/dist/js/bootstrap.min.js");
 
-
-import { useEffect } from "react";
-
+import Layout from '../Components/Layout';
 
 function MyApp({ Component, pageProps }) {
-
   return (
     <>
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
     </Head>
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <Provider store={store}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
     </>
   );
 }
 
-export default MyApp
+export default wrapper.withRedux(MyApp);
+// export default wrapper MyApp
 
 
